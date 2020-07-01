9f365c65cbd1cb598fa86d9000b7b8756356443a
Amit Sharma <amit.sharma@srijan.net>
2020-07-01 16:40:05 +0530

    Added logic to read multiple files.

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const core = require('@actions/core')
+const glob = require('@actions/glob')
 const { Toolkit } = require('actions-toolkit')
 const fm = require('front-matter')
 const nunjucks = require('nunjucks')
 const dateFilter = require('nunjucks-date-filter')
+const table = require('markdown-table')
+
 
 function listToArray (list) {
   if (!list) return []
@@ -12,6 +15,7 @@ function listToArray (list) {
 Toolkit.run(async tools => {
   const template = tools.inputs.filename || '.github/ISSUE_TEMPLATE.md'
   const assignees = tools.inputs.assignees
+  // var vulnerabilities = tools.inputs.vulnerabilities || './anchore-reports/vulnerabilities.json'
   const env = nunjucks.configure({ autoescape: false })
   env.addFilter('date', dateFilter)
 
@@ -21,7 +25,35 @@ Toolkit.run(async tools => {
     date: Date.now()
   }
 
-  // Get the file
+  // Read all the scan reports using glob.
+  glob("./anchore-reports/scan_*.json", function(err, files) { // read the folder.
+    files.forEach(function(vulnerabilities) {
+      // Get the vulnerability file
+      tools.log.debug('Reading vulnerabilities file', vulnerabilities)
+      const vulnerability_file_data = tools.getFile(vulnerabilities)
+      const vulnerabilities_data = JSON.parse(vulnerability_file_data)
+      // let severities = []
+      const issues = vulnerabilities_data.vulnerabilities
+      let table_data = [["Package", "version", "fix", "vulnerability", "Risk", "Scan_File_Path"]]
+      issues.forEach(issue => {
+        // let columns = []
+        if (issue.severity === "High" && issue.fix != "None"){
+          // severities.push(issue)
+          let vulnerability = `[${issue.vuln}](${issue.url})`
+          table_data.push([
+            issue.package_name,
+            issue.package_version,
+            issue.fix,
+            vulnerability,
+            issue.severity,
+            vulnerabilities
+          ]);
+        }
+      });
+    });
+  });
+
+  // Get the template file
   tools.log.debug('Reading from file', template)
   const file = tools.getFile(template)
 
@@ -29,39 +61,78 @@ Toolkit.run(async tools => {
   const { attributes, body } = fm(file)
   tools.log(`Front matter for ${template} is`, attributes)
 
+  // compose issue body
+  const issue_body = body + '\n' + table(table_data)
+
   const templated = {
-    body: env.renderString(body, templateVariables),
+    body: env.renderString(issue_body, templateVariables),
     title: env.renderString(attributes.title, templateVariables)
   }
 
   tools.log.debug('Templates compiled', templated)
   tools.log.info(`Creating new issue ${templated.title}`)
 
-  // Create the new issue
+  // read open issue created my action
+  // @TODO: here
+  var createNewIssue = TRUE;
   try {
-    const issue = await tools.github.issues.create({
-      ...tools.context.repo,
-      ...templated,
-      assignees: assignees ? listToArray(assignees) : listToArray(attributes.assignees),
-      labels: listToArray(attributes.labels),
-      milestone: tools.inputs.milestone || attributes.milestone
-    })
+    var openIssues = await tools.github.issues.listForRepo({
+      owner: 'srijanone',
+      repo: 'docker-base-images',
+      creator: 'github-actions[bot]',
+      state: 'open'
+    });
+    tools.log.info(open_issues);
 
-    core.setOutput('number', String(issue.data.number))
-    core.setOutput('url', issue.data.html_url)
-    tools.log.success(`Created issue ${issue.data.title}#${issue.data.number}: ${issue.data.html_url}`)
+    // Check if issue exists with same vulnerabilities.
+    openIssues.forEach(openIssue => {
+      if (base64_encode(openIssue.body) == base64_encode(templated.body)) {
+        createNewIssue = FALSE;
+        break;
+      }
+    });
+
   } catch (err) {
     // Log the error message
-    const errorMessage = `An error occurred while creating the issue. This might be caused by a malformed issue title, or a typo in the labels or assignees. Check ${template}!`
-    tools.log.error(errorMessage)
-    tools.log.error(err)
+    const errorMessage = `Error reading issues`;
+    tools.log.error(errorMessage);
+    tools.log.error(err);
 
     // The error might have more details
-    if (err.errors) tools.log.error(err.errors)
+    if (err.errors) tools.log.error(err.errors);
 
     // Exit with a failing status
-    core.setFailed(errorMessage + '\n\n' + err.message)
-    tools.exit.failure()
+    core.setFailed(errorMessage + '\n\n' + err.message);
+    tools.exit.failure();
+  }
+
+  // Create the new issue
+  if (createNewIssue == TRUE) {
+    try {
+      const issue = await tools.github.issues.create({
+        ...tools.context.repo,
+        ...templated,
+        assignees: assignees ? listToArray(assignees) : listToArray(attributes.assignees),
+        labels: listToArray(attributes.labels),
+        milestone: tools.inputs.milestone || attributes.milestone
+      })
+
+      core.setOutput('number', String(issue.data.number))
+      core.setOutput('url', issue.data.html_url)
+      tools.log.success(`Created issue ${issue.data.title}#${issue.data.number}: ${issue.data.html_url}`)
+    } catch (err) {
+      // Log the error message
+      const errorMessage = `An error occurred while creating the issue. This might be caused by a malformed issue title, or a typo in the labels or assignees. Check ${template}!`
+      tools.log.error(errorMessage)
+      tools.log.error(err)
+
+      // The error might have more details
+      if (err.errors) tools.log.error(err.errors)
+
+      // Exit with a failing status
+      core.setFailed(errorMessage + '\n\n' + err.message)
+      tools.exit.failure()
+    }
   }
 }, {
   secrets: ['GITHUB_TOKEN']
